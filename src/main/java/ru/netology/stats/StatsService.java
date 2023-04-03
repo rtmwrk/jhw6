c4a04917c381558bf586fae32367f0412d59e221
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-04-03 22:13:39 +0300

    Remove redundant code

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,32 +3,25 @@ package ru.netology.stats;
 public class StatsService {
 
     // Метод расчета суммы продаж за указанный период
-    public int calculateSumSales(int[] Sales) {
-        int amounts = 0;                                // Обнуляем счетчик сумм продаж
-        for (int month = 0; month < Sales.length; month++) {
-            amounts += Sales[month];                    // Увеличиваем сумму продаж на продажи за каждый месяц
+    public int calculateSumSales(int[] sales) {
+        int amounts = 0;
+        for (int month : sales) {
+            amounts += month;
         }
         return amounts;
     }
 
     // Метод расчета средней суммы продаж в месяц
-    public double calculateAwerageSumSalesInMonth(int[] Sales) {
-        // Считаем сумму продаж за указанный период
-        int amounts = 0;                                // Обнуляем счетчик сумм продаж
-        for (int month = 0; month < Sales.length; month++) {
-            amounts += Sales[month];                    // Увеличиваем сумму продаж на продажи за каждый месяц
-        }
-        // Считаем среднюю суммму продаж за месяц
-        double awerage = (amounts / Sales.length);
-
-        return awerage;
+    public double calculateAwerageSumSalesInMonth(int[] sales) {
+        int amounts = calculateSumSales(sales) / sales.length;
+        return amounts;
     }
 
     // Метод расчета месяца - пика продаж
-    public int calculateMonthMaxSales(int[] Sales) {
-        int indexMaxValue = 0;                          // Обнуляем индекс месяца с пиком продаж
-        for (int month = 0; month < Sales.length; month++) {
-            if (Sales[indexMaxValue] <= Sales[month]) {
+    public int calculateMonthMaxSales(int[] sales) {
+        int indexMaxValue = 0;
+        for (int month = 0; month < sales.length; month++) {
+            if (sales[indexMaxValue] <= sales[month]) {
                 indexMaxValue = month;
             }
         }
@@ -36,10 +29,10 @@ public class StatsService {
     }                                                   // В нашем массиве индексы, фактически номера месяцев считаются с "0"
 
     // Метод расчета месяца - минимум продаж
-    public int calculateMonthMinSales(int[] Sales) {
-        int indexMinValue = 0;                          // Обнуляем индекс месяца с минимумом продаж
-        for (int month = 0; month < Sales.length; month++) {
-            if (Sales[indexMinValue] >= Sales[month]) {
+    public int calculateMonthMinSales(int[] sales) {
+        int indexMinValue = 0;
+        for (int month = 0; month < sales.length; month++) {
+            if (sales[indexMinValue] >= sales[month]) {
                 indexMinValue = month;
             }
         }
@@ -47,36 +40,24 @@ public class StatsService {
     }                                                   // В нашем массиве индексы, фактически номера месяцев считаются с "0"
 
     // Метод расчета количества месяцев, в которых продажи ниже среднего значания
-    public int calculateAmountMonthWhereSalesLessAwerage(int[] Sales) {
-        // Считаем среднее значение продаж
-        int amounts = 0;                                // Обнуляем счетчик сумм продаж
-        for (int month = 0; month < Sales.length; month++) {
-            amounts += Sales[month];                    // Увеличиваем сумму продаж на продажи за каждый месяц
-        }
-        double awerage = (amounts / Sales.length);      // Расчет среднего значения продаж
-        // Считаем количество месяцев с продажами ниже среднего
+    public int calculateAmountMonthWhereSalesLessAwerage(int[] sales) {
+        double awerage = calculateAwerageSumSalesInMonth(sales);
         int amountsMonthLessAwerage = 0;
-        for (int month = 0; month < Sales.length; month++) {
-            if (Sales[month] < awerage) {
-                amountsMonthLessAwerage += 1;            // Увеличиваем счетчик месяцев, удовлетворяющих условию
+        for (int month : sales) {
+            if (month < awerage) {
+                amountsMonthLessAwerage += 1;
             }
         }
         return amountsMonthLessAwerage;                  // Возвращаем количество месяцев, в кот. продажи ниже среднего
     }
 
     // Метод расчета количества месяцев, в которых продажи выше среднего значания
-    public int calculateAmountMonthWhereSalesMoreAwerage(int[] Sales) {
-        // Считаем среднее значение продаж
-        int amounts = 0;                                // Обнуляем счетчик сумм продаж
-        for (int month = 0; month < Sales.length; month++) {
-            amounts += Sales[month];                    // Увеличиваем сумму продаж на продажи за каждый месяц
-        }
-        double awerage = (amounts / Sales.length);      // Расчет среднего значения продаж
-        // Считаем количество месяцев с продажами выше среднего
+    public int calculateAmountMonthWhereSalesMoreAwerage(int[] sales) {
+        double awerage = calculateAwerageSumSalesInMonth(sales);
         int amountsMonthMoreAwerage = 0;
-        for (int month = 0; month < Sales.length; month++) {
-            if (Sales[month] > awerage) {
-                amountsMonthMoreAwerage += 1;            // Увеличиваем счетчик месяцев, удовлетворяющих условию
+        for (int month : sales) {
+            if (month > awerage) {
+                amountsMonthMoreAwerage += 1;
             }
         }
         return amountsMonthMoreAwerage;                  // Возвращаем количество месяцев, в кот. продажи ниже среднего
